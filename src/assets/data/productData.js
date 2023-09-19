d89943e3834313e79355d66548669805996074fc
GmanSibs1906 <gmansibs@gmail.com>
2023-09-19 16:58:10 +0200

    pictures and products data

---
+++ src/assets/data/productData.js
@@ -0,0 +1,109 @@
+import {
+    bananaMuffin,
+    challahRolls,
+    cheeseCake,
+    chocolateCake,
+    frenchPastries,
+    fruitCheese,
+    garlicRolls,
+    homemadeCupcakes,
+    milkTart,
+    appleTart,
+    vanillaCupcake,
+    birthdayCake,
+    veganCupcake,
+} from "../images";
+
+export const products = [
+    {
+        name: "Banana Muffin",
+        image: bananaMuffin,
+        desc: "Delicious banana flavoured muffins",
+        price: 25,
+        cat: "cupCake"
+    },
+    {
+        name: "Challah Rolls",
+        image: challahRolls,
+        desc: "Freshly baked challah rolls",
+        price: 18,
+        cat: "bread"
+    },
+    {
+        name: "Wedding Cheese Cake",
+        image: cheeseCake,
+        desc: "Elegant wedding cheese cake",
+        price: 85,
+        cat: "cake"
+    },
+    {
+        name: "Chocolate Cake",
+        image: chocolateCake,
+        desc: "Rich and decadent chocolate cake",
+        price: 50,
+        cat: "cake"
+    },
+    {
+        name: "French Pastries",
+        image: frenchPastries,
+        desc: "Assortment of delicious French pastries",
+        price: 30,
+        cat: "bread"
+    },
+    {
+        name: "Fruit Cheese",
+        image: fruitCheese,
+        desc: "Creamy fruit-flavored cheese cake",
+        price: 22,
+        cat: "cake"
+    },
+    {
+        name: "Garlic Rolls",
+        image: garlicRolls,
+        desc: "Garlic-infused soft rolls",
+        price: 15,
+        cat: "bread"
+    },
+    {
+        name: "Homemade Cupcakes",
+        image: homemadeCupcakes,
+        desc: "Homemade assorted cupcakes",
+        price: 28,
+        cat: "cupCake"
+    },
+    {
+        name: "Milk Tart",
+        image: milkTart,
+        desc: "Classic South African milk tart",
+        price: 20,
+        cat: "pastry"
+    },
+    {
+        name: "Apple Tart",
+        image: appleTart,
+        desc: "Delicious apple tart with a flaky crust",
+        price: 24,
+        cat: "pastry"
+    },
+    {
+        name: "Vanilla Cupcake",
+        image: vanillaCupcake,
+        desc: "Classic vanilla-flavored cupcake",
+        price: 22,
+        cat: "cupCake"
+    },
+    {
+        name: "Birthday Cake",
+        image: birthdayCake,
+        desc: "Colorful birthday cake with sprinkles",
+        price: 40,
+        cat: "cake"
+    },
+    {
+        name: "Vegan Cupcake",
+        image: veganCupcake,
+        desc: "Delicious vegan-friendly cupcake",
+        price: 26,
+        cat: "cupCake"
+    },
+]
--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -3,9 +3,40 @@ import banner from "./baker.jpg";
 import wheat from "./wheat.png";
 import PastryChef from "./PastryChef.jpg";
 
+// products
+import bananaMuffin from "./banana-muffin.jpg";
+import challahRolls from "./challah-rolls.jpg";
+import cheeseCake from "./cheese-cake.jpg";
+import chocolateCake from "./chocolate-birthday-cake.jpg";
+import frenchPastries from"./french-pastries.png";
+import fruitCheese from "./fruit-and-cheese-wedding.jpg";
+import garlicRolls from "./garlic-roles.jpg";
+import homemadeCupcakes from "./homemade-cellow-cupcakes-.jpg";
+import milkTart from "./milk-tart.jpg";
+import appleTart from "./mini-apple-tart.jpg";
+import vanillaCupcake from "./vanilla-bean-cupcake.webp";
+import birthdayCake from "./vanilla-birthday-cake.webp";
+import veganCupcake from "./vegan-vanilla-cupcake.webp";
+
+
 export {
     // home banner 
     banner,
     wheat,
     PastryChef,
+    
+    // products
+    bananaMuffin,
+    challahRolls,
+    cheeseCake,
+    chocolateCake,
+    frenchPastries,
+    fruitCheese,
+    garlicRolls,
+    homemadeCupcakes,
+    milkTart,
+    appleTart,
+    vanillaCupcake,
+    birthdayCake,
+    veganCupcake,
 }
