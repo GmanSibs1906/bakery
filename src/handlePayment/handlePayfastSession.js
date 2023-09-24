fa2cce496942e2fd7422fae04767e218aebf4f68
Gladman Sibanda <73471539+GmanSibs1906@users.noreply.github.com>
2023-09-24 13:00:40 +0200

    Update payment

--- src/handlePayment/handlePayfastSession.js
+++ src/handlePayment/handlePayfastSession.js
@@ -1,25 +1,25 @@
 import { generatePreHash, generateSignature } from "./generatePaymentSignature";
 
-const passphrase = "helloworld1990"
-   const merchant_id =  "10027583"
-   const merchant_key = "ntokols4n2w5l"
-   const paymentURL = "https://sandbox.payfast.co.za/eng/process"
+const passphrase = ""
+   const merchant_id =  ""
+   const merchant_key = ""
+   const paymentURL = ""
 
-export const handlePayfastSession =  (orderId, totalPrice) => {
+export const handlePayfastSession =  () => {
 
     
 
     const process = {
-        merchant_id: merchant_id,
-        merchant_key: merchant_key,
-        return_url: `https://jovial-seahorse-ce30a6.netlify.app/`,
-        cancel_url: `https://portfolio-fa997.web.app/`,
-        notify_url: `https://jovial-seahorse-ce30a6.netlify.app/`,
+        merchant_id: ,
+        merchant_key: ,
+        return_url: ``,
+        cancel_url: ``,
+        notify_url: ``,
         name_first: "",
         email_address: "",
         m_payment_id: "",
-        amount: totalPrice,
-        item_name: orderId,
+        amount: "",
+        item_name:"" ,
         item_description: "",
         custom_int1: "",
         custom_str1: "",
